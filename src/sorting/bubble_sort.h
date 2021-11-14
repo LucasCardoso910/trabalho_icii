04c2e058f08ad3357a0b05f091d03f77f466e340
lucascardoso910 <lucascardoso910@gmail.com>
2021-11-14 17:13:32 -0300

    Create bubble_sort algorithm
    Signed-off-by: lucascardoso910 <lucascardoso910@gmail.com>

---
+++ src/sorting/bubble_sort.h
@@ -0,0 +1,21 @@
+#include "../library.h"
+
+char ** bubble_sort(char **array, int size){
+    int changed;
+    int count = 1;
+
+    do{
+        changed = FALSE;
+
+        for(int i = 0; i < size - count; i++) {
+            if (compare(array[i], array[i + 1]) > 0) {
+                swap(array[i], array[i + 1]);
+                changed = TRUE;
+            }
+        }
+
+        count++;
+    } while(changed);
+
+    return array;
+}
--- src/todo.md
+++ src/todo.md
@@ -2,5 +2,7 @@
 
 * Podemos mudar apenas o primeiro termo no righ_shift ?
 * Right shift e left shift contabilizam como movimentações?
+* Qual método da bolha implementar? Bubblesort ou shakesort ou tanto faz?
+* Podemos implementar variações diferentes dos algoritmos passados em aula?
 
 ## TODO:

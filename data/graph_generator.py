6c3b2bf6bc9fc2c3da664f62114a505e56f12704
lucascardoso910 <lucascardoso910@gmail.com>
2021-11-19 18:20:26 -0300

    Improve graph_generator.py
    Beautify graphs and improve code by creating a function. Also change the
    x_axis from the file name to how many lines it has.
    
    Signed-off-by: lucascardoso910 <lucascardoso910@gmail.com>
    Co-authored-by: Pauloguilhermepp <pauloguilherme@usp.br>

--- data/graph_generator.py
+++ data/graph_generator.py
@@ -1,54 +1,54 @@
 import matplotlib.pyplot as plt
 import os
 
-''' 
-mes_1, comparacoes, movimentos
-mes_2, comparacoes, movimentos
-mes_3, comparacoes, movimentos
-mes_4, comparacoes, movimentos
-mes_5, comparacoes, movimentos
-'''
+lines_in_file = {
+    "mes_1": 100,
+    "mes_2": 500,
+    "mes_3": 1000,
+    "mes_4": 5000,
+    "mes_5": 10000,
+}
 
-csv_files = os.listdir("files")
 
-for nome_do_arquivo in csv_files:
-    name, extension = nome_do_arquivo.split(".")
+def create_graph(x_axis, y_axis, model, name):
+    fig = plt.figure()
 
-    with open("files/" + nome_do_arquivo) as f:
-        arquivo = []
-        comparacoes = []
-        movimentos = []
-        
-        for line in f:
-            line = line.rstrip()
-            line = line.split(",")
+    plt.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
+    plt.ticklabel_format(axis="x", style="sci", scilimits=(0, 0))
 
-            arquivo.append(line[0])
-            comparacoes.append(int(line[1]))
-            movimentos.append(int(line[2]))
+    plt.scatter(x_axis, y_axis, color="blue")
 
-        #fig = plt.figure()
-        fig, ab = plt.subplots()
-        ab.ticklabel_format(style='plain') # para não ficar em notação científica
-        ax = plt.gca()
-        plt.bar(arquivo, comparacoes)
-        ax.set_ylim(0,1.01 * max(comparacoes)) # para definir o limite no eixo y
-        plt.title("Gráfico do número de comparações pelo arquivo - " + name)
-        plt.xlabel("Arquivo escolhido")
-        plt.ylabel("Número de comparações")
-        plt.grid()
-        # plt.show()
-        plt.savefig("graphs/" + name + "-comparacoes.png", format="png", dpi=fig.dpi)
+    plt.title(model.title() + " por arquivo - " + name)
+    plt.xlabel("Arquivo escolhido")
+    plt.ylabel("Número de " + model)
 
-        #fig = plt.figure()
-        fig, ab = plt.subplots()
-        ab.ticklabel_format(style='plain')
-        ax = plt.gca()
-        plt.bar(arquivo, movimentos)
-        ax.set_ylim(0,1.01 * max(movimentos))
-        plt.title("Gráfico do número de movimentações pelo arquivo - " + name)
-        plt.xlabel("Arquivo escolhido")
-        plt.ylabel("Número de movimentações")
-        plt.grid()
-        # plt.show()
-        plt.savefig("graphs/" + name + "-movimentacoes.png", format="png", dpi=fig.dpi)
+    plt.grid()
+
+    plt.savefig("graphs/" + name + "-" + model + ".png", format="png", dpi=fig.dpi)
+
+
+def main():
+    csv_files = os.listdir("files")
+
+    for nome_do_arquivo in csv_files:
+        name, _ = nome_do_arquivo.split(".")
+
+        with open("files/" + nome_do_arquivo) as f:
+            arquivo = []
+            comparacoes = []
+            movimentos = []
+
+            for line in f:
+                line = line.rstrip()
+                line = line.split(",")
+
+                arquivo.append(lines_in_file[line[0]])
+                comparacoes.append(int(line[1]))
+                movimentos.append(int(line[2]))
+
+        create_graph(arquivo, comparacoes, "comparações", name)
+        create_graph(arquivo, movimentos, "movimentos", name)
+
+
+if __name__ == "__main__":
+    main()

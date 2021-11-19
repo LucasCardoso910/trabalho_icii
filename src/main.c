2f549f84843305d8cfdb64a3be967afa402fdfb2
lucascardoso910 <lucascardoso910@gmail.com>
2021-11-19 16:50:18 -0300

    Create shake_sort algorithm
    Signed-off-by: lucascardoso910 <lucascardoso910@gmail.com>
    Co-authored-by: Pauloguilhermepp <pauloguilherme@usp.br>

--- src/main.c
+++ src/main.c
@@ -11,7 +11,8 @@ int main() {
     // Array with all sorting functions pointers
     char* (*sorting_functions[])(char**, int) = {
         &improved_bubble_sort,
-        &bubble_sort, 
+        &bubble_sort,
+        &shake_sort, 
         &insert_binary_sort, 
         &insert_sort, 
         &selection_sort,
@@ -26,7 +27,7 @@ int main() {
         sprintf(input_file, "input/%s.txt", month);
         sprintf(output_file, "output/%s.txt", month);
 
-        for (int j = 0; j < 7; j++) {
+        for (int j = 0; j < 8; j++) {
             array = read_file(input_file, &size);
             sort(*sorting_functions[j], array, &size, month);
         }
--- src/sorting/bubble_sort.h
+++ src/sorting/bubble_sort.h
@@ -33,3 +33,32 @@ char* improved_bubble_sort(char **array, int size) {
     return "improved_bubble_sort";
 }
 
+char* shake_sort(char **array, int size){
+    int left = 1;
+    int right = size - 1;
+    int k = size - 1;
+
+    do{
+        
+        for(int j = right; j>=left; j--){
+            if(compare(array[j-1],array[j]) > 0){
+                swap(array[j-1], array[j]);
+                k = j;
+            }
+        }
+
+        left = k + 1;
+
+        for (int j = left; j <= right; j++) {
+            if (compare(array[j - 1], array[j]) > 0) {
+                swap(array[j - 1], array[j]);
+                k = j;
+            }
+        }
+
+
+        right = k - 1;
+    }while(left <= right);
+
+    return "shake_sort";
+}

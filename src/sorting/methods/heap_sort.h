1e6ea01ad63fcc8fba40b7faf7312914d48c1abc
lucascardoso910 <lucascardoso910@gmail.com>
2021-11-27 10:47:54 -0300

    Improve heap_sort algorithm
    It is still not working just like Renato's. But I am each time more
    convinced that the numbers in the PDF are wrong. The algorithm is
    working just fine and I can't think of any more precise way to measure
    its comparisons and moves and yet the numbers are divergent.
    
    Signed-off-by: lucascardoso910 <lucascardoso910@gmail.com>

--- src/sorting/methods/heap_sort.h
+++ src/sorting/methods/heap_sort.h
@@ -1,20 +1,20 @@
 void heapify(char **array, int left, int right) {
     int i = left;
-    int j = 2 * left;
+    int j = (2 * left);
     char x[STRING_SIZE];
-    move(x, array[left]);
 
+    move(x, array[left]);
     if ((j < right) && (compare(array[j], array[j + 1]) < 0)) {
-        j = j + 1;
+        j++;
     }
 
     while ((j <= right) && (compare(x, array[j]) < 0)) {
         move(array[i], array[j]);
         i = j;
-        j = 2 * j;
+        j *= 2;
 
         if ((j < right) && (compare(array[j], array[j + 1]) < 0)) {
-            j = j + 1;
+            j++;
         }
     }
 
@@ -25,14 +25,18 @@ char *heap_sort(char **array, int size) {
     int left;
     int right;
 
-    for (left = size / 2; left >= 0; left--) {
+    array = right_shift(array, &size);
+
+    for (left = ((size - 1) / 2); left > 0; left--) {
         heapify(array, left, size - 1);
     }
 
-    for (right = size - 1; right > 0; right--) {
-        swap(array[0], array[right]);
-        heapify(array, 0, right - 1);
+    for (right = (size - 1); right > 1; right--) {
+        swap(array[1], array[right]);
+        heapify(array, 1, right - 1);
     }
+
+    array = left_shift(array, &size);
 
     return "heap_sort";
 }

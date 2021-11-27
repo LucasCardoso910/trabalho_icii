0926d29cd1f945702a066249ca4ee458742b1e63
lucascardoso910 <lucascardoso910@gmail.com>
2021-11-27 11:15:39 -0300

    Improving functions in search header
    Refactored some function, rewrote some sentences, simplified in general
    its actions and improved communication with the user.
    
    Signed-off-by: lucascardoso910 <lucascardoso910@gmail.com>

--- src/search/main.h
+++ src/search/main.h
@@ -3,25 +3,25 @@
 #include "methods/binary_search.h"
 
 void show_menu() {
-    printf("-------------MENU-------------\n");
+    printf("\n-------------MENU------------\n");
     printf("What do you want to do now?\n");
-    printf("01) Search for another code.\n");
+    printf("01) Search for a code.\n");
     printf("02) Close the program.\n");
+    printf("Insert your option: ");
 }
 
 void presentation() {
-    printf("Hello! I am a bot that will help you to find a code in an array of "
-           "codes.\n");
+    printf("Hello! I will help you to find a code in the files.\n");
 }
 
-void create_code(CODE *target, char *code, int index, int month) {
-    char month_str[MONTH_STR_SIZE];
+CODE create_code(char *code, int index, int month) {
+    CODE new_code;
 
-    sprintf(month_str, "mes_%d", month);
+    strcpy(new_code.code, code);
+    new_code.index = index;
+    new_code.month = month;
 
-    strcpy(target->code, code);
-    target->index = index;
-    strcpy(target->month, month_str);
+    return new_code;
 }
 
 CODE *expand_codes(CODE *array, int *size) {
@@ -31,25 +31,23 @@ CODE *expand_codes(CODE *array, int *size) {
     return array;
 }
 
-void search_for_repeated(int index, int limit, char ***data, int file,
-                         CODE *found_codes, int *results) {
-    int repeated = 0;
+void search_doubles(char **array, int start, int end, CODE *codes, int *size) {
+    int file;
+    int index = start;
 
     if (index == -1) {
         return;
     }
 
-    while (index + repeated < limit &&
-           strcmp(data[file][index], data[file][index + repeated]) == 0) {
-        found_codes = expand_codes(found_codes, results);
-        create_code(&found_codes[*results - 1], data[file][index],
-                    index + repeated + 1, file + 1);
-        repeated++;
+    file = codes[*size - 1].month;
+    while (index < end && strcmp(array[start], array[index]) == 0) {
+        index++;
+        codes = expand_codes(codes, size);
+        codes[*size - 1] = create_code(array[start], index, file);
     }
 }
 
-CODE *search_code(char ***data, int size[FILES_QTT], char key[STRING_SIZE],
-                  int *results) {
+CODE *search_code(char ***data, int size[], char key[], int *results) {
     CODE *found_codes;
     CODE new_code;
     int index;
@@ -62,22 +60,44 @@ CODE *search_code(char ***data, int size[FILES_QTT], char key[STRING_SIZE],
         repeated = 0;
         index = binary_search(data[file], size[file], key);
 
-        search_for_repeated(index, size[file], data, file, found_codes,
-                            results);
+        if (index != -1) {
+            found_codes = expand_codes(found_codes, results);
+            found_codes[*results - 1] = create_code(key, index, file + 1);
+        }
+        search_doubles(data[file], index, size[file], found_codes, results);
     }
 
     return found_codes;
 }
 
 void print_code(CODE *found_codes, int size) {
+    // To know if the current code and the previous monthes are differente
+    int month_previous = -1;
+    int month_counter = 0;
+    int total_counter = 0;
 
-    if (size > 0) {
-        printf("The key \"%s\" appears on:\n", found_codes[0].code);
-        for (int i = 0; i < size; i++) {
-            printf("Line #%d in the %s file\n", found_codes[i].index,
-                   found_codes[i].month);
-        }
-    } else {
+    if (size == 0) {
         printf("This key was not found in any file!\n");
+        return;
     }
+
+    printf("\nThe key \"%s\" appears on:\n", found_codes[0].code);
+    for (int i = 0; i < size; i++) {
+        if (found_codes[i].month != month_previous) {
+            if (month_previous != -1) {
+                printf("Subtotal: %d\n", month_counter);
+            }
+
+            printf("\n");
+            printf("In file mes_%d:\n", found_codes[i].month);
+
+            month_counter = 0;
+            month_previous = found_codes[i].month;
+        }
+        printf("- Line #%d\n", found_codes[i].index);
+        month_counter++;
+        total_counter++;
+    }
+    printf("Subtotal: %d\n", month_counter);
+    printf("Total: %d occurences\n", total_counter);
 }
